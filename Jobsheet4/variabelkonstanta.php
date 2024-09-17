0b71e4f226c1def16eef56f077a4e9d66313d29b
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-09-17 08:46:11 +0700

    js04 - prac 1

--- Jobsheet4/variabelkonstanta.php
+++ Jobsheet4/variabelkonstanta.php
@@ -3,4 +3,15 @@ $angka1 = 10;
 $angka2 = 5;
 $hasil = $angka1 + $angka2;
 echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
+
+$benar = true;
+$salah = false;
+echo "Variabel benar: $benar, Variabel salah: $salah";
+
+// Mendefinisian konstanta untuk nilai tetap
+define("NAMA_STATUS", "WebsiteKu.com");
+define("TAHUN_PENDIRIAN", 2023);
+
+echo "Selamat datang di " . NAMA_STATUS . ", situs yang dididrikan
+pada tahun " . TAHUN_PENDIRIAN . ".";
 ?>
