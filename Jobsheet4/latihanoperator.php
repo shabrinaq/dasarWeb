c9485e212ab9ddef24453fe501623562b82ba1bd
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-09-17 18:20:12 +0700

    js04 - prac 3

--- Jobsheet4/latihanoperator.php
+++ Jobsheet4/latihanoperator.php
@@ -1,1 +1,12 @@
 <?php
+
+$totalSeats = 45;
+$occupiedSeats = 28;
+$emptySeats = $totalSeats - $occupiedSeats;
+$persentaseEmptySeats = ($emptySeats / $totalSeats) * 100;
+
+echo "Total Seats: {$totalSeats} <br>";
+echo "Occupied Seats: {$occupiedSeats} <br>";
+echo "Empty Seat: {$emptySeats} <br>";
+echo "Persentase Empty Seats: {$persentaseEmptySeats} <br>";
+?>
