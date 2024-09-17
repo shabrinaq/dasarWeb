98df53ba4ff310f449164a1049506d8a9737575c
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-09-17 18:56:43 +0700

    js04 - prac 5

--- Jobsheet4/array.php
+++ Jobsheet4/array.php
@@ -11,4 +11,23 @@ foreach ($nilaiSiswa as $nilai) {
 }
 
 echo "Daftar nilai siswa yang lulus: " . implode(', ', $nilaiLulus);
+
+$daftarKaryawan = [
+    ['Alice', 7],
+    ['Bob', 3],
+    ['Charlie', 9],
+    ['David', 5],
+    ['Eva', 6],
+];
+
+$karyawanPengalamanLimaTahun = [];
+
+foreach ($daftarKaryawan as $karyawan) {
+    if ($karyawan[1] > 5) {
+        $karyawanPengalamanLimaTahun[] = $karyawan[0];
+    }
+}
+echo "<br>";
+echo "Daftar karyawan dengan pengalaman kerja lebih dari 5 tahun: " . implode(', ',
+$karyawanPengalamanLimaTahun);
 ?>
