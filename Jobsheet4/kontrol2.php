e33785149e2868683ed4defe2432aa2b0c7fcc4b
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-09-17 20:40:27 +0700

    js04 - prac 4

--- Jobsheet4/kontrol2.php
+++ Jobsheet4/kontrol2.php
@@ -1,1 +1,14 @@
 <?php
+
+$hargaAwal = 120000;
+$diskonPersen = 20;
+
+if ($hargaAwal > 100000) {
+    $jumlahDiskon = $hargaAwal * ($diskonPersen / 100);
+    $hargaSetelahDiskon = $hargaAwal - $jumlahDiskon;
+} else {
+    $hargaSetelahDiskon = $hargaAwal;
+}
+echo "<br>";
+echo "Harga setelah diskon: Rp " . number_format($hargaSetelahDiskon, 0, ',', '.');
+?>
