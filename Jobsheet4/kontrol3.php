49e415b76ba8ec44fc38bae3de537a4ccc4e4a56
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-09-17 20:47:18 +0700

    js04 - prac 4

--- Jobsheet4/kontrol3.php
+++ Jobsheet4/kontrol3.php
@@ -1,1 +1,11 @@
 <?php
+$poin = 700;
+
+echo "Player's total score is: {$poin} <br>";
+
+if ($poin > 500) {
+    echo "Do players get additional rewards? YES";
+} else {
+    echo "Do players get additional rewards? NO";
+}
+?>
