e8d3c4d83b904ae7097f61437a781ee89ac460b7
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-09-24 10:22:57 +0700

    JS05_PHP2 - Prac 4

---
+++ JS05_PHP-2/function_return.php
@@ -0,0 +1,10 @@
+<?php
+
+function hitungUmur($thn_lahir, $thn_sekrang){
+    $umur = $thn_sekrang - $thn_lahir;
+    return $umur;
+}
+
+echo "umur saya adalah ". hitungUmur(1988, 2023) ."tahun"
+
+?>
