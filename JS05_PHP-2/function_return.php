f462a55669175b9571f8054dc1bd110fd530966b
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-09-24 10:30:13 +0700

    JS05_PHP2 - Prac 4

--- JS05_PHP-2/function_return.php
+++ JS05_PHP-2/function_return.php
@@ -4,7 +4,13 @@ function hitungUmur($thn_lahir, $thn_sekrang){
     $umur = $thn_sekrang - $thn_lahir;
     return $umur;
 }
+function perkenalan ($nama, $salam="Assalamualaikum") {
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
 
-echo "umur saya adalah ". hitungUmur(1988, 2024) ."tahun"
+    echo "Saya berusia ". hitungUmur(1988, 2024) ." tahun<br/>";
+}
+
+perkenalan("Elok");
 
 ?>
