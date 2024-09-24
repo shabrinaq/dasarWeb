3381068ecb4c800bd526f103578f6b31d468cfd0
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-09-24 10:37:21 +0700

    JS05_PHP2 - Prac 5

--- JS05_PHP-2/recursive.php
+++ JS05_PHP-2/recursive.php
@@ -1,9 +1,6 @@
 <?php
-function tampilkanHaloDunia(){
-    echo "Halo dunia! <br>";
-
-    tampilkanHaloDunia();
+for ($i=1; $i <=25; $i++) { 
+    echo "Perulangan ke-{$i} <br>";
 }
 
-tampilkanHaloDunia();
 ?>
