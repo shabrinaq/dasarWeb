facad0e6cc4282f60cfa3e92e3dabf77a30673ae
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-09-24 10:15:10 +0700

    JS05_PHP2 - Prac 4

--- JS05_PHP-2/function.php
+++ JS05_PHP-2/function.php
@@ -1,11 +1,17 @@
 <?php
 
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Elok</br>";
+function perkenalan($nama, $salam){
+    echo $salam. ", ";
+    echo "Perkenalkan, nama saya " . $nama . "<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
 
-perkenalan();
-perkenalan();
+perkenalan("Hamdana", "Hallo");
+
+echo "<hr>";
+
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+
+perkenalan($saya, $ucapanSalam);
 ?>
