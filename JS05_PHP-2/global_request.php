3767790f0fa9e98dec5709f4d48adb4ca398b8c9
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-09-25 19:17:34 +0700

    JS05_PHP2 - Prac 11

--- JS05_PHP-2/global_request.php
+++ JS05_PHP-2/global_request.php
@@ -2,7 +2,7 @@
     <body>
 
     <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-        Name: <input type="text" name="frame">
+        Name: <input type="text" name="fname">
         <input type="submit">
     </form>
 
