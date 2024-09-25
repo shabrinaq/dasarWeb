493f8901e58df5a8187b8fec70f72947f82cf615
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-09-25 19:04:11 +0700

    JS05_PHP2 - Prac 11

--- JS05_PHP-2/global_request.php
+++ JS05_PHP-2/global_request.php
@@ -8,7 +8,7 @@
 
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $name = $_POST['fname'];
+        $name = $_REQUEST['fname'];
         if (empty($name)) {
             echo "Name is empty";
         } else {
@@ -16,6 +16,6 @@
         }
     }
     ?>
-
+    
     </body>
 </html>
