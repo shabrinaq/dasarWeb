3a7640ce8836e8abacc8a96233db8ed5e1fb7278
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-09-25 18:58:56 +0700

    JS05_PHP2 - Prac 11

---
+++ JS05_PHP-2/global_post.php
@@ -0,0 +1,21 @@
+<html>
+    <body>
+
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        Name: <input type="text" name="frame">
+        <input type="submit">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST['frame'];
+        if (empty($name)) {
+            echo "Name is empty";
+        } else {
+            echo $name;
+        }
+    }
+    ?>
+    
+    </body>
+</html>
