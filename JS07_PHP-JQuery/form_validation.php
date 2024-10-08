21a661b6e9c261d4f3781639f1435cf85e56f830
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-10-08 18:31:23 +0700

    JS07_PHP-JQuery_Prac 7

--- JS07_PHP-JQuery/form_validation.php
+++ JS07_PHP-JQuery/form_validation.php
@@ -44,11 +44,6 @@
                         event.preventDefault();
                     }
                 });
-
-                function validateEmail(email) {
-                    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+&/;
-                    return re.test(String(email).toLowerCase());
-                }
             });
         </script>
     </body>
