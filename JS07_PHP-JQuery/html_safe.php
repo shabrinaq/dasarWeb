5eb28d32c50b17b520d060e77d0abc9f3f56510e
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-10-08 15:49:47 +0700

    JS07_PHP-JQuery_Prac 4

--- JS07_PHP-JQuery/html_safe.php
+++ JS07_PHP-JQuery/html_safe.php
@@ -1,6 +1,48 @@
 <?php
-$input = $_POST['input'];
-$input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $input = isset($_POST['input']) ? $_POST['input'] : '';
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
 
-echo "<script>alert('You have been hacked!');</script>";
+    if ($input != '') {
+        $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+    }
+
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $user_input = isset($_POST['user_input']) ? htmlspecialchars($_POST['user_input'], ENT_QUOTES, 'UTF-8') : '';
+        echo '<div>' . $user_input . '</div>';
+    } else {
+        echo '<div>Invalid email address.</div>';
+    }
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>HTML Safe Input</title>
+    </head>
+    <body>
+        <form method="POST" action="">
+            <label for="input">Input:</label>
+            <input type="text" id="input" name="input" required>
+
+            <br>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+
+            <br>
+
+            <label for="user_input">User Input:</label>
+            <input type="text" id="user_input" name="user_input">
+
+            <br>
+            <br>
+            
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
+
