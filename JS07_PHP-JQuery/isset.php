f04bdfd0dcc29f6a2b78d71e24b4d6371c9fe4c3
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-10-08 08:30:31 +0700

    JS07_PHP-JQuery_Prac 1

--- JS07_PHP-JQuery/isset.php
+++ JS07_PHP-JQuery/isset.php
@@ -5,11 +5,4 @@ if (isset($umur) && $umur>= 18) {
 } else {
     echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.";
 }
-
-$data = array("nama" => "Jane", "usia" => 25);
-if (isset($data["nama"])) {
-    echo "Nama: " . $data["nama"];
-} else {
-    echo "Variabel 'nama' tidak ditemukan dalam array.";
-}
 ?>
