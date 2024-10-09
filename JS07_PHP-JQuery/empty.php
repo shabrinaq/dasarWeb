0896a87aa198cb1df6864e55ab0b6a051245d5f7
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-10-09 20:14:40 +0700

    JS07_PHP-JQuery_Prac 2

--- JS07_PHP-JQuery/empty.php
+++ JS07_PHP-JQuery/empty.php
@@ -1,12 +1,12 @@
 <?php
 $myArray = array();
 if (empty($myArray)) {
-    echo "Array tidak terdefinisi atau kosong.";
+    echo "Array tidak terdefinisi atau kosong." . "<br>";
 } else {
     echo "Array terdefinisi dan tidak kosong.";
 }
 if (empty($nonExistentVar)) {
-    echo "Variabel tidak terdefinisi atau kosong.";
+    echo "Variabel tidak terdefinisi atau kosong." . "<br>";
 } else {
     echo "Variabel terdefinisi dan tidak kosong.";
 }
