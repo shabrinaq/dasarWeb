ac019eb1b85614f29c92c54ee2e91f33935cf209
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-10-15 10:17:21 +0700

    JS08 - Prac 1_Question 6 & 7

--- JS08_PHP-FormUpload,CookiesAndSession/upload.php
+++ JS08_PHP-FormUpload,CookiesAndSession/upload.php
@@ -1,28 +1,20 @@
 <?php
 if (isset($_POST["submit"])) {
-    $targetdir = "uploads/"; 
-    $fileName = $_FILES['myfile']['name']; 
-    $fileTmpName = $_FILES['myfile']['tmp_name']; 
-    $fileSize = $_FILES['myfile']['size']; 
-    $fileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION)); 
+    $targetdir = "uploads/"; // Direktori tujuan untuk menyimpan file
+    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]); 
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION)); 
 
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-    $maxsize = 5 * 1024 * 1024; 
+    $allowedExtensions = array("txt", "pdf", "doc", "docx"); 
+    $maxsize = 3 * 1024 * 1024; 
 
-    if ($_FILES['myfile']['error'] == 0) {
-        if (in_array($fileType, $allowedExtensions) && $fileSize <= $maxsize) {
-            if (move_uploaded_file($fileTmpName, $targetdir . $fileName)) {
-                echo "File berhasil diunggah.<br>";
-                // Menampilkan gambar yang diunggah
-                echo "<img src='$targetdir$fileName' width='200' alt='Thumbnail'>";
-            } else {
-                echo "Gagal memindahkan file ke direktori tujuan.";
-            }
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+            echo "File berhasil diunggah.";
         } else {
-            echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
+            echo "Gagal mengunggah file.";
         }
     } else {
-        echo "Gagal mengunggah file. Error: " . $_FILES['myfile']['error'];
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
     }
 }
 ?>
