8b3165f58463ab073390e7dd60845aa23e4987f9
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-10-15 09:20:03 +0700

    JS08 - Prac 1

--- JS08_PHP-FormUpload,CookiesAndSession/upload.php
+++ JS08_PHP-FormUpload,CookiesAndSession/upload.php
@@ -2,11 +2,20 @@
 if (isset($_POST["submit"])) {
     $targetdir = "uploads/"; //DIrektori tujuan untuk menyimpan file
     $targetfile = $targetdir . basename($_FILES["myfile"]["tmp_name"]);
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-        echo "File berhasil diunggah.";
-    } else {
-        echo "Gagal mengunggah file.";
-    }  
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $maxsize = 5*1024*1024;
+
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"]<=$maxsize) {
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+            echo "File berhasil diunggah.";
+        } else {
+            echo "Gagal mengunggah file.";
+        }  
+    }
+    else {
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
+    }
 }
 ?>
