def60ed19a60642686b2ac3e4e79008f939a6bbd
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-11-04 23:55:33 +0700

    JS10 - PHP Crud_Prac 2 (soal no.2)

---
+++ JS10_PHP-Crud/create.php
@@ -0,0 +1,28 @@
+<IDOCTYPE html>
+<html>
+    <head>
+        <title>Tambah Data Anggota</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="container">
+            <h2>Tambah Data Anggota</h2>
+            <form action="proses.php?aksi=tambah" method="post">
+                <label for="nama">Nama: </label>
+                <input type="text" name="nana" id="nama" required>
+                <label for="jenis_kelamin">Jenis Kelamin: </label>
+                <div class="radio-group">
+                <input type="radio" name="jenis_kelamin" value="L" id="laki" required><label for="laki">Laki-laki</label>
+                <input type="radio" name="jenis_kelanin" value="P" id="perempuan" required><label for="perempuan">
+    Perempuan</label>
+            </div>
+                <label for="alamat">Alamat: </label>
+                <input type="text" name="alamat" id="alanat" required>
+                <label for="no_telp">No. Telp :</label>
+                <input type="text" name="no_telp" id="no_telp" required>
+                <button type="submit">Simpan Data</button>
+                <a href="index.php" class="btn-kembali">Kembali</a>
+            </form>
+        </div>
+    </body>
+</html>
