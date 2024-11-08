5f7e4e4d5c708a0bef143df8ff4cea17cf79f2b9
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-11-09 00:18:51 +0700

    JS11 - PHP

--- JS11_PHP-StructuralDynamicWeb/tamplate/footer.php
+++
@@ -1,8 +0,0 @@
-<script src="assets/dist/js/bootstrap.bundle.min.js"></script>
-
-<script src="https://cdn. jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/
-njoSsxfbrkCTnJrzXt+ENP5MOVBxD+16sEG4zoLp" crossorigin="anonymous"></script>
-<script src="assets/custom/dashboard. js"></script>
-</body>
-
-</html>
