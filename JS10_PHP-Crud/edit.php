e0944d3d8c0323b7902d7cfbb8abdb137cea502b
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-11-10 14:49:43 +0700

    JS10_PHP-Crud_Prac 5 (Question No.5.3)

--- JS10_PHP-Crud/edit.php
+++ JS10_PHP-Crud/edit.php
@@ -1,35 +1,54 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Edit Member Data</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <?php
-        include('koneksi.php');
-        $id = $_GET['id'];
-        $query = "SELECT * FROM member WHERE id = $id";
-        $result = mysqli_query($koneksi, $query);
-        $row = mysqli_fetch_assoc($result);
-        mysqli_close($koneksi);
-        ?>
-        <div class="container">
-            <h2>Edit Member Data</h2>
-            <form action="proses.php?action=edit" method="post">
-                <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+<head>
+    <title>Edit Member Data</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <?php
+    include('koneksi.php');
+    $id = $_GET['id'];
+    $query = "SELECT * FROM member WHERE id = $id";
+    $result = mysqli_query($koneksi, $query);
+    $row = mysqli_fetch_assoc($result);
+    mysqli_close($koneksi);
+    ?>
+
+    <div class="container mt-4">
+        <h2>Edit Member Data</h2>
+        <form action="proses.php?action=edit" method="post">
+            <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+            <div class="form-group">
                 <label for="name">Name:</label>
-                <input type="text" name="name" id="name" value="<?php echo $row['name']; ?>" required>
+                <input type="text" class="form-control" name="name" id="name" value="<?php echo $row['name']; ?>" required>
+            </div>
+            <div class="form-group">
                 <label for="gender">Gender:</label>
-                <div class="radio-group">
-                    <input type="radio" name="gender" value="L" id="male" <?php if ($row['gender'] === 'L') echo 'checked'; ?> required><label for="male">Male</label>
-                    <input type="radio" name="gender" value="P" id="female" <?php if ($row['gender'] === 'P') echo 'checked'; ?> required><label for="female">Female</label>
+                <div class="form-check">
+                    <input type="radio" class="form-check-input" name="gender" value="L" id="male" <?php if ($row['gender'] == 'L') echo 'checked'; ?> required>
+                    <label class="form-check-label" for="male">Male</label>
                 </div>
+                <div class="form-check">
+                    <input type="radio" class="form-check-input" name="gender" value="P" id="female" <?php if ($row['gender'] == 'P') echo 'checked'; ?> required>
+                    <label class="form-check-label" for="female">Female</label>
+                </div>
+            </div>
+            <div class="form-group">
                 <label for="address">Address:</label>
-                <input type="text" name="address" id="address" value="<?php echo $row['address']; ?>" required>
+                <input type="text" class="form-control" name="address" id="address" value="<?php echo $row['address']; ?>" required>
+            </div>
+            <div class="form-group">
                 <label for="phone_number">Phone Number:</label>
-                <input type="text" name="phone_number" id="phone_number" value="<?php echo $row['phone_number']; ?>" required>
-                <button type="submit">Save Changes</button> <a href="index.php" class="btn-kembali">Back</a>
-            </form>
-        </div>
-    </body>
+                <input type="text" class="form-control" name="phone_number" id="phone_number" value="<?php echo $row['phone_number']; ?>" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Save Changes</button>
+        </form>
+
+        <a class="btn btn-secondary mt-2" href="index.php">Back</a>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
 </html>
