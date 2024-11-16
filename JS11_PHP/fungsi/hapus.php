63fbdea94bb35535884db73c96124355f7c92bfb
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-11-16 23:41:25 +0700

    JS11-PHP_Prac 5 (Question No. 5.2)

--- JS11_PHP/fungsi/hapus.php
+++ JS11_PHP/fungsi/hapus.php
@@ -4,6 +4,7 @@ if (!empty($_SESSION['username'])) {
     require '../config/koneksi.php';
     require '../fungsi/pesan_kilat.php';
     require '../fungsi/anti_injection.php';
+    
     if (!empty($_GET['jabatan'])) {
         $id = antiinjection($koneksi, $_GET['id']);
         $query = "DELETE FROM jabatan WHERE id = '$id'";
@@ -13,5 +14,19 @@ if (!empty($_SESSION['username'])) {
             pesan('danger', "Jabatan Tidak Terhapus Karena: " . mysqli_error($koneksi));
         }
         header("Location: ../index.php?pagejabatan");
+    } elseif (!empty($_GET['anggota'])) {
+        $id = antiinjection($koneksi, $_GET['id']);
+        $query = "DELETE FROM user WHERE id = '$id'";
+        if (mysqli_query($koneksi, $query)) {
+            $query2 = "DELETE FROM user WHERE id = '$id'";
+            if (mysqli_query($koneksi, $query2)) {
+                pesan('success', "Anggota Telah Terhapus.");
+            } else {
+                pesan('warning', "Data Login Terhapus Tetapi Data Anggota Tidak Terhapus Karena: " . mysqli_error($koneksi));
+            }  
+        } else {
+            pesan('danger', "Anggota Tidak Terhapus Karena: " . mysqli_error($koneksi));
+        }
+        header("location: ../index.php?page=anggota");
     }
 }
