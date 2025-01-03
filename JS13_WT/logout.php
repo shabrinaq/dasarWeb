23b2b7b3e17281f21628d0919a96a4dc1ebb652e
shabrinaq <shabrinaqottrunnada@gmail.com>
2025-01-03 07:23:47 +0700

    JS13_WT

---
+++ JS13_WT/logout.php
@@ -0,0 +1,26 @@
+<?php
+session_start(); // Mulai sesi
+// Hapus semua variabel sesi
+$_SESSION = array();
+
+// Hapus cookie sesi jika ada
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
+// Hancurkan sesi
+session_destroy();
+
+// Arahkan pengguna ke halaman login
+header('Location: login.php');
+exit();
+?>
